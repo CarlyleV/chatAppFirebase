284fa04b47b29a94db0c06c68e63674a221a0958
CarlyleV <85601247+CarlyleV@users.noreply.github.com>
2021-08-17 23:16:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ import "firebase/database"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkmQ12qMiCzrYS7MAW9cfZoqASDW1odOc",
-    authDomain: "vue-chat-36815.firebaseapp.com",
-    databaseURL:"vue-chat-36815-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "vue-chat-36815",
-    storageBucket: "vue-chat-36815.appspot.com",
-    messagingSenderId: "14261960732",
-    appId: "1:14261960732:web:006d4003f4e22cdea85fd9"
+    apiKey: "",
+    authDomain: "",
+    databaseURL:"",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
